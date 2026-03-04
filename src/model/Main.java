5334bec5f1efc97d0c9904659472482f306b241e
CN-Alu <chris03valerioo@gmail.com>
2026-03-04 11:29:01 +0100

    TaskService y main

---
+++ src/model/Main.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TaskService taskService = new TaskService();
+		Scanner input = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("\n--- TASK MANAGER ---");
+			System.out.println("1. Crear Tarea");
+			System.out.println("2. Listar Tareas");
+			System.out.println("3. Completar Tarea");
+			System.out.println("4. Eliminar Tarea");
+			System.out.println("5. Salir");
+			System.out.println("Opción: ");
+			
+			int option = input.nextInt();
+			input.nextLine();
+			
+			switch(option) {
+			case 1:
+				System.out.println("\n--- Crear Tarea ---");
+				System.out.println("Titulo: ");
+				String title =  input.nextLine();
+				taskService.addTask(title);
+				System.out.println("Press Enter to Continue");
+				input.nextLine();
+				break;
+			case 2:
+				System.out.println("\n--- Lista de Tareas ---");
+				taskService.listTasks();
+				System.out.println("Press Enter to Continue");
+				input.nextLine();
+				break;
+			case 3:
+				System.out.println("\n--- Completar Tarea ---");
+				System.out.println("Introduce ID de la tarea:");
+				int	completeId = input.nextInt();
+				taskService.completeTask(completeId);
+				System.out.println("Press Enter to Continue");
+				input.nextLine();
+				break;
+			case 4:
+				System.out.println("\n--- Eliminar Tarea ---");
+				System.out.println("Introduce ID de la tarea:");
+				int	deleteId = input.nextInt();
+				taskService.deleteTask(deleteId);
+				System.out.println("Press Enter to Continue");
+				input.nextLine();
+				break;
+			case 5:
+				System.out.println("Hasta la Próxima...");
+				System.out.println("Press Enter to Continue");
+				input.nextLine();
+				input.close();
+				return;
+			default:
+				System.out.println("Opción no válida.");
+				System.out.println("Press Enter to Continue");
+				input.nextLine();
+			}
+		}
+	}
+
+}
---
+++ src/model/TaskService.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskService {
+	
+	private List<Tasks> tasks = new ArrayList<Tasks>();
+	private int nextId = 1;
+	
+	public void addTask(String title) {
+		Tasks task = new Tasks(nextId++, title);
+		tasks.add(task);
+	}
+	
+	public void listTasks() {
+		if (tasks.isEmpty()) {
+			System.out.println("No hay tareas");
+			return;
+		}
+		
+		for (Tasks task : tasks) {
+			System.out.println(task);
+		}
+	}
+	
+	public void completeTask(int id) {
+		for (Tasks task : tasks) {
+			if (task.getId() == id) {
+				task.markAsCompleted();
+				return;
+			}
+		}
+		System.out.println("Tarea no encontrada.");
+	}
+	
+	public void deleteTask(int id) {
+		for (Tasks task : tasks) {
+			if (task.getId() == id) {
+				tasks.remove(id);
+				return;
+			}
+		}
+		System.out.println("Tarea no encontrada.");
+	}
+}
