359ade057ab6ea4b87bad659a8b9ff2ef731e518
CN-Alu <chris03valerioo@gmail.com>
2026-08-10 20:15:41 +0200

    Add AbstractDaoImpl, users entity, and diagrams
    add AbstractDaoImpl, users placeholder, and architecture diagrams

---
+++ TASKMANAGER/src/main/java/dao/AbstractDaoImpl.java
@@ -0,0 +1,23 @@
+package dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+
+public class AbstractDaoImpl {
+
+	protected EntityManagerFactory emf;
+	protected EntityManager em;
+	protected EntityTransaction tx;
+	protected Query query;
+	protected String jpql;
+	
+	
+	public AbstractDaoImpl() {
+		emf = Persistence.createEntityManagerFactory("TASKMANAGER");
+		em = emf.createEntityManager();
+		tx = em.getTransaction();
+	}
+}
