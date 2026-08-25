5abbc81f17a997d256a50a4f1b04db8ec27a341c
CN-Alu <chris03valerioo@gmail.com>
2026-08-25 23:54:11 +0200

    Link users to projects in DAO test

--- src/main/java/testr/TestProyectoDao.java
+++ src/main/java/testr/TestProyectoDao.java
@@ -1,18 +1,25 @@
 package testr;
 
 import dao.IProyectoDao;
+import dao.IUsuarioDao;
 import dao.ProyectoDaoImpl;
+import dao.UsuarioDaoImpl;
 import entities.Proyecto;
+import entities.Usuario;
 
 public class TestProyectoDao {
 
 	private static IProyectoDao pdao;
+	private static IUsuarioDao udao;
 	
 	static {
 		pdao = new ProyectoDaoImpl();
+		udao = new UsuarioDaoImpl();
 	}
 	
 	public static void main(String[] args) {
+		
+		insertOnUsuarioProyecto(1L, 1L);
 		leerTodos();
 		//crearProyecto();
 		//eliminarProyecto(14L);
@@ -22,9 +29,19 @@ public class TestProyectoDao {
 	}
 	
 	public static void leerTodos() {
-		for (Proyecto p : pdao.findAll())
+		for (Proyecto p : pdao.findAll()) {
 			System.out.println(p);
+		}
 	}
 	
+	public static void insertOnUsuarioProyecto(Long usuarioId, Long proyectoId) {
+		Usuario usuario = udao.findById(usuarioId);
+		Proyecto proyecto = pdao.findById(proyectoId);
+		
+		proyecto.getUsuarios().add(usuario);
+		
+		pdao.updateOne(proyecto);
+		System.out.println(proyecto);
+	}
 
 }
